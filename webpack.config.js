fe485ac589be6add943ce89199c582732ae51c4e
Ricky Patel <rickyp@snowcoders.com>
2018-08-06 18:27:02 -0700

    Fixed resize detector

--- webpack.config.js
+++ webpack.config.js
@@ -34,48 +34,28 @@ const config /*: webpack.Configuration*/ = {
   // Specifically because we use umd, we have to specify more information
   // https://stackoverflow.com/questions/34252424/webpack-umd-lib-and-external-files
   externals: {
-    "@snowcoders/react-unstyled-button": {
-      amd: "@snowcoders/react-unstyled-button",
-      commonjs: "@snowcoders/react-unstyled-button",
-      commonjs2: "@snowcoders/react-unstyled-button",
-      root: "@snowcoders/react-unstyled-button"
-    },
+    "@snowcoders/react-unstyled-button": "@snowcoders/react-unstyled-button",
     "react-dom": {
       amd: "react-dom",
       commonjs: "react-dom",
       commonjs2: "react-dom",
       root: "ReactDOM"
     },
-    "react-popper": {
-      amd: "react-popper",
-      commonjs: "react-popper",
-      commonjs2: "react-popper",
-      root: "react-popper"
-    },
+    "react-popper": "react-popper",
     "react-resize-detector": {
       amd: "react-resize-detector",
       commonjs: "react-resize-detector",
       commonjs2: "react-resize-detector",
       root: "ResizeDetector"
     },
-    classnames: {
-      amd: "classnames",
-      commonjs: "classnames",
-      commonjs2: "classnames",
-      root: "classnames"
-    },
+    classnames: "classnames",
     react: {
       amd: "react",
       commonjs: "react",
       commonjs2: "react",
       root: "React"
     },
-    tslib: {
-      amd: "tslib",
-      commonjs: "tslib",
-      commonjs2: "tslib",
-      root: "tslib"
-    }
+    tslib: "tslib"
   }
 };
 
